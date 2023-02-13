9fd51458aff8e9092b366a20111cf4cfc035d4c8
Victor Ubere <victorubere@gmail.com>
2023-02-13 13:53:31 +0100

    test: framer motion animatepresence for routers

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,43 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const variants = {
+    initialLoad: {
+      opacity: 0,  
+      y: "-100vh"
+    },
+    animateLoad: {
+      opacity: 1, 
+      y:0
+    },
+    exitLoad: {
+      opacity: 0,
+      y: "-100vh"
+    },
+  };
+  console.log(router.route)
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.section
+          key={router.route}
+          initial="initialLoad"
+          animate="animateLoad"
+          exit="exitLoad"
+          transition={{
+            duration: 0.5,
+          }}
+          variants={variants}
+        >
+          <Component {...pageProps} />
+        </motion.section>
+      </AnimatePresence>
     </Layout>
   );
 }
