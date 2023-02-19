fca54bd677832efc1b0bfe7c1222d9d76147be2c
Victor Ubere <victorubere@gmail.com>
2023-02-19 19:06:47 +0100

    feat: contact page error handling

---
+++ components/modal/index.jsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect } from "react";
+
+export default function Modal({children,  ref }) {
+
+
+  return (
+    <AnimatePresence>
+      <motion.div
+        ref={ref}
+        className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0)] z-50"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="w-full h-full flex items-center justify-center">
+          <motion.div
+            className="w-[90vw] h-[100px] bg-[#f3f3f3] rounded-lg max-w-[200px] shadow-[1px_1px_2px_2px_rgba(0,0,0,0.4)] flex flex-col items-center justify-center p-2"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+                {children}  
+          </motion.div>
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
--- pages/contact-me/index.jsx
+++ pages/contact-me/index.jsx
@@ -1,49 +1,103 @@
-import Head from "next/head";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
+/* components */
+import Head from "next/head";
+import Modal from "../../components/modal";
+import { useCloseOnBlur } from "../../hooks/useCloseonClickAway";
 
 export default function ContactMe() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const form = useRef()
-  console.log(process.env.SERVICE_ID)
-  const submitHandler = async(e) => {
+  const form = useRef();
+  const [status, setStatus] = useState("");
+  
+
+  const {
+    ref: successRef,
+    show: showSuccess,
+    setShow: setShowSuccess,
+  } = useCloseOnBlur();
+  const {
+    ref: errorRef,
+    show: showError,
+    setShow: setShowError,
+  } = useCloseOnBlur();
+  const { ref: loadingRef, show: showLoading, setShow: setShowLoading } = useCloseOnBlur();
+
+  const emailPattern =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    
+    setShowLoading(true);
     try {
-      const emailPattern =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
       if (name.length < 2) {
-        throw "Please enter a valid name";
+        throw { text: "Please enter a valid name" };
       }
       if (!emailPattern.test(email)) {
-        throw "Please enter a valid email address";
+        throw { text: "Please enter a valid email address" };
       }
       if (message.length < 2) {
-        throw "Please enter a valid message";
+        throw { text: "Please enter a valid message" };
       }
-      
-      
-      const res = await emailjs.send(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, {
-        from_name: name,
-        message: message,
-        from_email: email,
-        reply_to: email,
-      }, process.env.NEXT_PUBLIC_KEY);
-      alert('Message sent successfully')
+      await emailjs.send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        {
+          from_name: name,
+          message: message,
+          from_email: email,
+          reply_to: email,
+        },
+        process.env.NEXT_PUBLIC_KEY
+      );
+      setShowLoading(false);
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 2000);
+
       setName("");
       setEmail("");
       setMessage("");
     } catch (err) {
-      alert(err, "message not sent");
+      setShowLoading(false);
+      setStatus(err?.text);
+      setTimeout(() => {
+        setStatus("");
+        setShowError(false);
+      }, 2000);
+      setShowError(true);
       return;
     }
   };
 
+  const disabled =
+    name.length < 2 || message.length < 2 || !emailPattern.test(email);
+
   return (
     <>
+      {showSuccess && (
+        <Modal setShow={setShowSuccess} ref={successRef}>
+          <p className="text-[#0f0] text-center ">Message sent successfully</p>
+        </Modal>
+      )}
+      {showError && (
+        <Modal setShow={setShowError} ref={errorRef}>
+          <p className="text-center leading-[18px] text-[#f00]">
+            {status || "Message not sent, check your network and retry."}
+          </p>
+        </Modal>
+      )}
+      {showLoading && (
+        <Modal  setShow={setShowLoading} ref={loadingRef}>
+          <p className="text-center leading-[18px] text-[#000f]">
+            Sending message...
+          </p>
+        </Modal>
+      )}
       <Head>
         <title>Contact Victor Ubere</title>
       </Head>
@@ -77,13 +131,14 @@ export default function ContactMe() {
             id="message"
             name="message"
             placeholder="Your message..."
-            className="border border-[#0007] text-[14px] w-[90%] rounded-[3px] p-2 m-2 placeholder:text-[#0009] placeholder:text-[12px] focus:outline-none focus:ring-1 focus:ring-[#0008] resize-none h-[100px] p-3"
+            className="border border-[#0007] text-[14px] w-[90%] rounded-[3px] px-2 m-2 placeholder:text-[#0009] placeholder:text-[12px] focus:outline-none focus:ring-1 focus:ring-[#0008] resize-none h-[100px] p-3"
             onChange={({ target }) => setMessage(target.value)}
-          
           />
           <button
             type="submit"
-            className="px-4 py-1 border rounded-[3px] bg-[#66f6] hover:bg-[#66f] text-white"
+            className={`px-4 py-1 border rounded-[3px] ${
+              disabled ? "bg-[#66f8]" : "bg-[#66f]"
+            } hover:bg-[#66f] text-white`}
           >
             Send
           </button>
