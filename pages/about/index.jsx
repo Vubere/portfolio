47c1ab6e543372b8ebf49aed6de436b820c9d805
Victor Ubere <victorubere@gmail.com>
2023-02-19 08:44:49 +0100

    Update about me

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -7,6 +7,13 @@ export default function Aside({ className }) {
     <section>
       <h2>About Me</h2>
       
+Victor Ubere is a frontend web developer and HNGi9 finalist with a passion for creating elegant and user-friendly web experiences. Born on June 2nd, 2000, Victor has been honing his skills in web development for several years, specializing in technologies like React, Tailwind, SASS, Firebase, Redux, and TypeScript. Victor is always eager to learn new programming languages and frameworks, and enjoys staying up-to-date with the latest trends in web development.
+
+In addition to his work as a frontend developer, Victor is also an avid chess player. He loves the strategic challenge of the game, and often finds inspiration in its emphasis on planning, analysis, and forward thinking. When he's not coding or playing chess, Victor enjoys spending time with family and friends.
+
+
+As a frontend developer, I am responsible for building and maintaining the client-side of web applications. I am proficient in React, Tailwind, SASS, Firebase, Redux and TypeScript, and I am always eager to learn new technologies and programming languages. I am skilled in translating UI/UX design wireframes into functional and responsive web pages, ensuring that the website is optimized for speed, accessibility, and user experience. Additionally, I am familiar with modern software development practices such as version control, automated testing, and continuous integration, which help me to deliver high-quality and scalable code. I am passionate about creating elegant and user-friendly web experiences, and I am always looking for ways to improve my skills and deliver the best results for my clients. 
+
 
     </section>
   );
